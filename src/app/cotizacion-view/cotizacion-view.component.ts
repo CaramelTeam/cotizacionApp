783ec74545da6dd072d7383dc57336d4fb250c24
Juan Avila <Juan_Avila3579360@jabil.com>
2023-10-06 16:49:59 -0600

    Avance en logica para angular

--- src/app/cotizacion-view/cotizacion-view.component.ts
+++ src/app/cotizacion-view/cotizacion-view.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cotizacion-view',
   templateUrl: './cotizacion-view.component.html',
   styleUrls: ['./cotizacion-view.component.css']
 })
-export class CotizacionViewComponent {
 
+export class CotizacionViewComponent  {
+ @Input() usersName!: string;
 }
